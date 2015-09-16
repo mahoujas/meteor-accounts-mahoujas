7bbd9215a96bfbbd58119ecafeed7036a95cd610
Pavan Kumar <pavan.kumar@mahoujas.com>
2015-09-16 15:38:58 +0530

    version 1.0.0

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+    name: "mahoujas:accounts-mahoujas",
+    summary: "Login service for Mahoujas accounts",
+    version: "1.0.0",
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.1.0.2');
+    api.use('accounts-base', ['client', 'server']);
+    // Export Accounts (etc) to packages using this one.
+    api.imply('accounts-base', ['client', 'server']);
+    api.use('accounts-oauth', ['client', 'server']);
+    api.use('mahoujas:mahoujas@1.0.0', ['client', 'server']);
+
+    api.addFiles('mahoujas_login_button.css', 'client');
+
+    api.addFiles("mahoujas.js");
+});
